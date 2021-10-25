991653e8a60228c588b34c8b3e8014e27f70a13e
Виктор Ли <lee.vitya28@gmail.com>
2021-10-25 15:35:46 +0600

    finished main part

---
+++ AppJogerTracker/Helpers/CoreHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  CoreHelpers.swift
+//  AppJogerTracker
+//
+//  Created by Victor Lee on 24/10/21.
+//
+
+import Foundation
---
+++ AppJogerTracker/Helpers/UIView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extension.swift
+//  AppJogerTracker
+//
+//  Created by Victor Lee on 24/10/21.
+//
+
+import Foundation
